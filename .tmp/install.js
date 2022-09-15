fac44112faa4cbbadc8d115546adc7ca44b6a7b9
madajaju <darren@pulsipher.org>
2022-09-14 20:30:20 -0700

    Updates to documentation and fixes to the deployment model.

--- .tmp/install.js
+++ .tmp/install.js
@@ -2,14 +2,13 @@
 
 const program = require('commander');
 
-const action = require('C:/Users/dwpulsip/AppData/Roaming/npm/node_modules/ailtire/src/interface/app/build.js');
+const action = require('C:/Users/dwpulsip/AppData/Roaming/npm/node_modules/ailtire/src/interface/app/install.js');
 const ActionHandler = require('C:/Users/dwpulsip/AppData/Roaming/npm/node_modules/ailtire/src/Server/Action.js');
 global.ailtire = { config: require('C:/Users/dwpulsip/AppData/Roaming/npm/node_modules/ailtire/bin/lib/../../.ailtire.js') };
 program
 	.requiredOption('--env <string>', 'Environment to Build')
-	.option('--name <string>', 'Name of the Build')
-	.option('--repo <string>', 'undefined')
-	.option('--recursive <boolean>', 'undefined');
+	.option('--name <string>', 'Name of the Installation')
+	.option('--repo <string>', 'Name of the Repository for the docker images');
 program.parse(process.argv);
 let results = ActionHandler.execute(action,program.opts(), {});
 console.log(results);
