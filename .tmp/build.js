d606e7dc06ef0df0942437effba3cb64915024c9
madajaju <darren@pulsipher.org>
2023-03-04 04:33:02 -0800

    Updates to documentation and fixes to the deployment model.

--- .tmp/build.js
+++ .tmp/build.js
@@ -2,14 +2,13 @@
 
 const program = require('commander');
 
-const action = require('C:/Users/dwpulsip/IdeaProjects/sabr/node_modules/ailtire/src/interface/app/build.js');
-const ActionHandler = require('C:/Users/dwpulsip/IdeaProjects/sabr/node_modules/ailtire/src/Server/Action.js');
-global.ailtire = { config: require('C:/Users/dwpulsip/IdeaProjects/sabr/node_modules/ailtire/bin/lib/../../.ailtire.js') };
+const action = require('C:/Users/darre/work/sabr/node_modules/ailtire/src/interface/package/build.js');
+const ActionHandler = require('C:/Users/darre/work/sabr/node_modules/ailtire/src/Server/Action.js');
+global.ailtire = { config: require('C:/Users/darre/work/sabr/node_modules/ailtire/bin/lib/../../.ailtire.js') };
 program
 	.requiredOption('--env <string>', 'Environment to Build')
-	.option('--name <string>', 'Name of the Build')
-	.option('--repo <string>', 'undefined')
-	.option('--recursive <boolean>', 'undefined');
+	.requiredOption('--name <string>', 'Name Package to build')
+	.option('--recursive <boolean>', 'Rescurse to all sub packages');
 program.parse(process.argv);
 let results = ActionHandler.execute(action,program.opts(), {});
 console.log(results);
